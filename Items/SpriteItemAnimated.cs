15812d8afaa6cb09d4262f8bd90bd4e37eb772cb
Teddi Lupus <l.valeria.lupa@gmail.com>
2024-09-26 01:01:33 -0400

    hate it here fr run the right one... (fixes command to work with new data set)

--- Items/SpriteItemAnimated.cs
+++ Items/SpriteItemAnimated.cs
@@ -12,7 +12,7 @@ namespace LegendOfZelda
         List<Rectangle> spriteFrames;
         int currentFrame = 0;
         int totalFrames;
-        double timePerFrame = 0.25; // Adjustable data
+        double timePerFrame = 0.3; // Adjustable data
         double timeElapsed = 0;
         int index;
         private int width;
@@ -22,7 +22,7 @@ namespace LegendOfZelda
         {
             itemTexture = texture;
             index = sIndex;
-            spriteFrames = SpriteItemData2.GetRectangleData(index);
+            spriteFrames = SpriteItemData.GetRectangleData(index);
             totalFrames = spriteFrames.Count;
         }
 
