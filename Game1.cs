9d7e33b37873de3e4974fbd0577915a66c5dd818
TeddyLupus <l.valeria.lupa@gmail.com>
2024-11-07 20:30:05 -0500

    Revert "Merging the parsing into most updated so I can work on a most up to date branch"

--- Game1.cs
+++ Game1.cs
@@ -1,20 +1,6 @@
-using Microsoft.Xna.Framework;
+﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
-using ObjectManagementExamples;
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-using LegendOfZelda.Collision;
-using System.Xml;
-using Microsoft.VisualBasic;
-using System.Diagnostics;
-using Microsoft.Xna.Framework.Audio;
-using LegendOfZelda.Sounds;
-using LegendOfZelda.HUD;
 
 namespace LegendOfZelda
 {
@@ -22,183 +8,43 @@ namespace LegendOfZelda
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
-        public Texture2D texture;
-        private KeyboardCont keyboardController;
-        public ArrayList sprites;
-        ArrayList controllerList;
-
-        // Later these enemies will be moved out to level loader.
-        public int currentSprite { get; set; }
-        public Texture2D linkTexture;
-        public Texture2D itemTexture;
-        public Texture2D blockTexture;
-        public Texture2D Bossture;
-        public Texture2D BackgroundTure;
-        public Texture2D HUDTexture;
-        public Block block;
-        public Link LinkCharacter;
-        private List<ILinkItem> inventory = new List<ILinkItem>();
-        public List<ClassItems> items = new List<ClassItems>();
-        
-        public List<IEnemy> enemies = new List<IEnemy>();
-        private List<ICollideable> blocks;
-        private List<ICollideable> movers;
-        private ISprite background;
-        private ISprite walls;
-        private IController controllerK;
-        private HUDManager hudManager;
-        //For collisions
-        detectionManager collisionDetector;
-        CollisionHandler collHandler;
-
-        SoundMachine soundMachine = SoundMachine.Instance;
 
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
-            _graphics.PreferredBackBufferWidth = Constants.ScreenWidth;
-            _graphics.PreferredBackBufferHeight = Constants.ScreenHeight;
         }
-        
+
         protected override void Initialize()
         {
-            controllerList = new ArrayList();
-            sprites = new ArrayList();
-            // Need discuss where to initialize
-            keyboardController = new KeyboardCont(this);
-            // Initializes keyboard controller
+            // TODO: Add your initialization logic here
 
-            controllerK = new KeyboardCont(this);
-            // TEMP
-            //init the collision stuff
-            collHandler = new CollisionHandler();
-            collisionDetector = new detectionManager(collHandler);
-            
             base.Initialize();
         }
 
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
-            // Temp load font for fps check.
-            // font = Content.Load<SpriteFont>("font");
-            // Load the texture for the sprite 
 
-
-            itemTexture = Content.Load<Texture2D>("itemSpriteFinal");
-            texture = Content.Load<Texture2D>("enemySpriteSheet");
-            Bossture = Content.Load<Texture2D>("bossSpriteSheet");
-            BackgroundTure = Content.Load<Texture2D>("ZeldaTileSheet");
-            ItemSpriteFactory.Instance.LoadAllTextures(Content);
-            EnemySpriteFactory.Instance.LoadAllTextures(Content);
-            BlockSpriteFactory.Instance.LoadAllTextures(Content);
-            HUDSpriteFactory.Instance.LoadAllTextures(Content);
-            hudManager = new HUDManager(this);
-            // TODO: Get absorbed by Level Loader as well so it can support custom backgrounds and walls
-            background = new Sprite(BackgroundTure, new List<Rectangle>() { new Rectangle(1, 192, 192, 112) });
-            walls = new Sprite(BackgroundTure, new List<Rectangle>() { new Rectangle(521, 11, 256, 176) });
-
-            // TODO: Make this fully within level loader. Not yet added b/c it would mess up a lot of commands and we don't have time to fix it rn
-            LinkSpriteFactory.Instance.LoadAllTextures(Content);
-            LinkCharacter = new Link();
-
-            LevelLoader.Instance.Load("Room1.xml");
-            RoomObjectManager.Instance.addLink(LinkCharacter);
-            RoomObjectManager.Instance.Update();
-
-            blocks = LevelLoader.Instance.getBlocks();
-            movers = LevelLoader.Instance.getMovers();
-
-            //I should probably be moving the sound loading to level loader.
-            SoundEffect attack = Content.Load<SoundEffect>("mikuAttack");
-            SoundEffect hurt = Content.Load<SoundEffect>("mikuHurt");
-            SoundEffect ha = Content.Load<SoundEffect>("mikuHa");
-            soundMachine.addSound("attack", attack);
-            soundMachine.addSound("hurt", hurt);
-            soundMachine.addSound("ha", ha);
-
-           
-            SoundEffect mikuSong = Content.Load<SoundEffect>("mikuSong");
-            SoundEffectInstance modifier = mikuSong.CreateInstance();
-            modifier.IsLooped = true;
-            modifier.Play();
-
-         
+            // TODO: use this.Content to load your game content here
         }
 
         protected override void Update(GameTime gameTime)
         {
-            blocks = LevelLoader.Instance.getBlocks();
-            movers = LevelLoader.Instance.getMovers();
-            // Let the keyboard controller handle input
-            keyboardController.Update();
-            RoomObjectManager.Instance.Update();
-            hudManager.Update(gameTime);
-            // Ensure the sprite is correctly referenced
-            foreach (IController controller in controllerList)
-            {
-                controller.Update();
-            }
-            // update collision
-            collisionDetector.update();
-            //collHandler.update();
-            //Update the current enemy to have the correct sprite and draw it
-            // The enemies use their own sprite batch so this must be outside the other sprite batch begin.
-            foreach (ICollideable mover in LevelLoader.Instance.getMovers())
-            {
-                mover.Update(gameTime);
-            }
-            //Update the keyboard controller
-            controllerK.Update();
-            
-            base.Update(gameTime);
-            // Calls link update, which updates his Sprite and Items
-            // LinkCharacter.Update(gameTime);
-            // Updates sprites in Item classes
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
 
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
         }
 
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.White);
-
+            GraphicsDevice.Clear(Color.CornflowerBlue);
 
             // TODO: Add your drawing code here
-            // Temp fps check.
-            double frameRate = 1 / gameTime.ElapsedGameTime.TotalSeconds;
-            string fpsText = $"FPS: {frameRate:0.00}";
-
-            var matrix = Matrix.CreateTranslation(0, Constants.HUDHeight, 0) * Matrix.CreateScale(Constants.ScaleX, Constants.ScaleY, 1.0f);
-
-            // Draw the game content with the transform matrix applied
-            _spriteBatch.Begin(SpriteSortMode.Deferred, null, SamplerState.PointClamp, transformMatrix: matrix);
-
-            walls.Draw(_spriteBatch, new Rectangle(0, 0, 800, 480), Color.White);
-            background.Draw(_spriteBatch, new Rectangle(100, 88, 600, 305), Color.White);
-            foreach (ICollideable block in blocks)
-            {
-                block.Draw(_spriteBatch);
-            }
-            foreach (ICollideable mover in LevelLoader.Instance.getMovers())
-            {
-                mover.Draw(_spriteBatch);
-            }
-            // Calls Link's Draw method
-            LinkCharacter.Draw(_spriteBatch);
-
-            //draw the dropped items
-            foreach (ClassItems statItem in RoomObjectManager.Instance.getGroundItems())
-            {
-                statItem.Draw(_spriteBatch);
-            }
-            hudManager.Draw(_spriteBatch);
-
-
-
-            // _spriteBatch.DrawString(font, fpsText, new Vector2(680,0), Color.White);
-            _spriteBatch.End();
 
             base.Draw(gameTime);
         }
--- README.md
+++ README.md
@@ -1,16 +1,1 @@
 # LegendOfZelda
-
-Current Sprint2 necessary documentation:
-Known bugs: 
-- Sometimes when a you throw a boomerang, it will spawn way behind the player and instantly collide with a wall, causing weird interactions, same with arrow.
-- Some enemies will linger after death
-- Some enemies do not die
-- There is a very very very small chance that a Goriya can clip into the wall and spin in a circle in perpetuity.
-- Room 14 is otherwise inaccessible and is currently bugged with how LevelLoader works
-- Stalfos is missing a frame
-- Goriya doesn't have a second movement frame when moving up and down
-
-Controls:
-Use WASD to move, 1 to throw a boomerang, 2 to shoot an arrow, 3 to throw fire, 4 to use the sword, and 5 to place a bomb.
-Use "enter" key to rotate through doors
-Press "q" key to quit game.
