7dc16cc5ecb861d20fd3d336a8194731bd461095
TeddyLupus <l.valeria.lupa@gmail.com>
2024-11-09 01:19:08 -0500

    Weapons on the ground can now be picked up and used, Press 3 to rotate the 2 items Miku uses to attack. HOWEVER, they still don't show up in the HUD and IDK why.

---
+++ Command/RotateItems.cs
@@ -0,0 +1,15 @@
+﻿
+namespace LegendOfZelda
+{
+    internal class RotateItems : ICommand
+    {
+
+        public RotateItems() 
+        {
+        }
+        public void Execute()
+        {
+            Link.Instance.inventory.rotateItems();
+        }
+    }
+}
